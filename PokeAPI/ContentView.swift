693cb5ce2039fc4ad94e8934083e80f41051e073
Robert Sandru <sandrurobert@gmail.com>
2022-10-27 13:34:47 +0300

    Fetching pokes, listing them on the screen

--- PokeAPI/ContentView.swift
+++ PokeAPI/ContentView.swift
@@ -10,16 +10,33 @@ import PokeAPI_Lib
 
 struct ContentView: View {
     
-    let service: PokemonRestService = PokemonRestService()
+    @State var paginatedResults: PaginatedResult<PokemonSnapshot> = PaginatedResult.emptyPage
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ScrollView {
+                ForEach(paginatedResults.results, id:\.self) { pokemon in
+                    VStack(alignment: .leading) {
+                        Text(pokemon.name)
+                        Text(pokemon.url)
+                    }
+                    .padding(.bottom)
+                }
+            }
         }
         .padding()
+        .onAppear {
+            let service: PokemonRestService = PokemonRestService()
+            let task = Task {
+                let result = await service.getPokemonPaginated(offset:10, limit: 10)
+                switch result {
+                case .success(let success):
+                    self.paginatedResults = success
+                case .failure(let failure):
+                    print(failure)
+                }
+            }
+        }
     }
 }
 
