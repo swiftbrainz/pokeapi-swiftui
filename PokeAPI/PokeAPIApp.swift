9e571b38898273417e86f88b4159dcb4e4f9bc12
Robert Sandru <sandrurobert@gmail.com>
2022-10-27 10:17:03 +0300

    Initial Commit

---
+++ PokeAPI/PokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPIApp.swift
+//  PokeAPI
+//
+//  Created by Robert Sandru on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
